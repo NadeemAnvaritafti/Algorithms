2a88be4ee371355a9c905033be796bd2b25bf15d
Nadeem <nanvari@live.com>
2020-03-18 17:47:00 -0500

    add rock_paper_scissors algorithm -- not complete

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,23 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    choices = ['rock', 'paper', 'scissors']
+    new_array = []
+
+    if n == 1:
+        for i in choices:
+            new_array.append([i])
+
+    return new_array
 
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+print(rock_paper_scissors(1))
+
+# if __name__ == "__main__":
+#     if len(sys.argv) > 1:
+#         num_plays = int(sys.argv[1])
+#         print(rock_paper_scissors(num_plays))
+#     else:
+#         print('Usage: rps.py [num_plays]')
